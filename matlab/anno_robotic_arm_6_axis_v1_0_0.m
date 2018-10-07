ebc31cfb616a7f336002450ab839cce23bf53310
WUT_Ruige_Lee <295054118@whut.edu.cn>
2018-10-07 16:21:26 +0800

    模型注入参数

--- matlab/anno_robotic_arm_6_axis_v1_0_0.m
+++ matlab/anno_robotic_arm_6_axis_v1_0_0.m
@@ -9,10 +9,18 @@ Pb = [1,0,0,0;  0,1,0,0;    0,0,1,0;    0,0,0,1];
 
 theta1 = pi/2;
 theta2 = pi/2;
+theta3 = 0;
+theta4 = 0;
+theta5 = 0;
+theta6 = 0;
 
-Ts_b = [cos(theta1) ,-sin(theta1),0,0;  sin(theta1),cos(theta1),0,0;    0,0,1,0;  0,0,0,1];
+Ts_b = [cos(theta1),-sin(theta1),0,0;	sin(theta1),cos(theta1),0,0;    0,0,1,0;  0,0,0,1];
 Ta_s = [cos(theta2),0,sin(theta2),0;    0,1,0,0;    -sin(theta2),0,cos(theta2),264;  0,0,0,1];
-Te_a = [1,0,0,0;    0,1,0,0;    0,0,1,225;    0,0,0,1];
+Te_a = [cos(theta3),0,sin(theta3),0;    0,1,0,0;    -sin(theta3),0,cos(theta3),225;    0,0,0,1];
+Tw_e = [cos(theta4),-sin(theta4),0,0;   sin(theta4),cos(theta4),0,0;    0,0,1,80;   0,0,0,1];
+Tf_w = [cos(theta5),0,sin(theta5),0;    0,1,0,0;    -sin(theta5),0,cos(theta5),137;  0,0,0,1];
+Tt_f = [cos(theta6),-sin(theta6),0,0;   sin(theta6),cos(theta6),0,0;    0,0,1,40;   0,0,0,1];
+Ttail= [1,0,0,0;    0,1,0,0;    0,0,1,0;    0,0,0,1];
 
 Pb*Ts_b* Ta_s*Te_a
 
