c8736e7321f1047b73fc7d52dad2a74e47251f7d
WUT_Ruige_Lee <295054118@whut.edu.cn>
2018-10-11 17:06:59 +0800

    第一步原子化

---
+++ matlab/anno_robotic_arm_6_axis_v2_3_0.m
@@ -0,0 +1,92 @@
+% 安诺6轴机械臂建模
+% 武汉理工大学
+% 李锐戈
+
+clc;
+clear;
+
+%aim point
+
+AIM_X = 100;
+AIM_Y = -100;
+AIM_Z = 200;
+
+%syms AIM_X AIM_Y AIM_Z;
+
+TB_tail =[1,0,0,AIM_X;  0,1,0,AIM_Y;  0,0,1,AIM_Z;  0,0,0,1]; 
+
+%tool_x = 0;
+%tool_y = 0;
+%tool_z = 14;
+
+syms tool_x tool_y tool_z;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%DH_TAB = [ 0,0,0,theta1;    -pi/2,0,0,theta2;   0,225,0,theta3; -pi/2,0,217.3,theta4;   pi/2,0,0,theta5;    -pi/2,0,0,theta6 ];
+
+PB = [1,0,0,0;  0,1,0,0;    0,0,1,0;    0,0,0,1];   %固定参数
+
+TB_0 = [1,0,0,0;    0,1,0,0;    0,0,1,264;  0,0,0,1];   %固定参数
+T6_t = [1,0,0,tool_x;    0,1,0,tool_y;    0,0,1,40+tool_z;   0,0,0,1];    %固定参数
+R6_T = T6_t(1:3,1:3);                                   %固定参数
+RT_6 = R6_T';                                           %固定参数
+
+T0_B = [1,0,0,0;    0,1,0,0;    0,0,1,-264; 0,0,0,1];   %固定参数
+
+Ttail_6 = [1,0,0,0; 0,1,0,0;    0,0,1,-54;    0,0,0,1]; %固定参数
+%Ttail_6 = [ RT_6, -RT_6 * T6_t(1:3,4);0,0,0,0 ];
+
+T0_6 = T0_B * TB_tail * Ttail_6;
+
+P06 = T0_6(:,4)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+theta1_result = atan( (AIM_Y) / (AIM_X) );
+
+
+SIN_THETA3 =  (225^2 +  217.3^2 - (AIM_X)^2 - (AIM_Y)^2 -  (AIM_Z - 318)^2 ) / ( 2*225*217.3 );
+
+theta3_result = asin(SIN_THETA3);
+
+%如何判定？
+u = (217.3 * sin(theta3_result) - 225) / ((AIM_Z - 318) - 217.3  * cos(theta3_result)) - sqrt( ((217.3*sin(theta3_result) -225)  / ( (AIM_Z - 318) - 217.3 * cos(theta3_result)))^2 - (((AIM_Z - 318)+217.3*cos( theta3_result ))/((AIM_Z - 318)-217.3*cos(theta3_result))) );
+
+theta2_result = atan(u)  * 2 ;
+
+
+
+T01 = [cos(theta1_result),-sin(theta1_result),0,0;    sin(theta1_result),cos(theta1_result),0,0;                    0,0,1,0;                          0,0,0,1];
+T12 = [cos(theta2_result),-sin(theta2_result),0,0;	0,0,1,0;    -sin(theta2_result),-cos(theta2_result),0,0;          0,0,0,1];
+T23 = [cos(theta3_result),-sin(theta3_result),0,225;  sin(theta3_result),cos(theta3_result),0,0;                 	0,0,1,0;                          0,0,0,1];
+
+%验算前几点
+P6_0 =T01 * T12 * T23 * [0;217.3;0;1]
+
+T0_3 = T01 * T12 *T23;
+R0_6 = T0_6(1:3,1:3);
+R0_3 = T0_3(1:3,1:3);
+R3_0 = R0_3';
+R3_6 = R3_0 * R0_6;
+
+theta5_result = acos(  R3_6(3,3));
+
+R45 = [cos(theta5_result),0,sin(theta5_result);    0,1,0;  -sin(theta5_result),0,cos(theta5_result)];
+
+theta6_result = -atan( ( R3_6(3,2) ) / ( R3_6(3,1) ) );
+theta4_result = atan ( R3_6(2,3) /  R3_6(1,3) );
+
+
+
+%%%验算
+
+TB_0 = [1,0,0,0;    0,1,0,0;    0,0,1,264;  0,0,0,1];
+T6_t = [1,0,0,0;    0,1,0,0;    0,0,1,40+14;   0,0,0,1];
+T01 = [cos(theta1_result),-sin(theta1_result),0,0;    sin(theta1_result),cos(theta1_result),0,0;                    0,0,1,0;                          0,0,0,1];
+T12 = [cos(theta2_result),-sin(theta2_result),0,0;	0,0,1,0;    -sin(theta2_result),-cos(theta2_result),0,0;          0,0,0,1];
+T23 = [cos(theta3_result),-sin(theta3_result),0,225;  sin(theta3_result),cos(theta3_result),0,0;                 	0,0,1,0;                          0,0,0,1];
+T34 = [cos(theta4_result),-sin(theta4_result),0,0;    0,0,1,217.3;    -sin(theta4_result),-cos(theta4_result),0,0;      0,0,0,1];
+T45 = [cos(theta5_result),-sin(theta5_result),0,0;    0,0,-1,0;    sin(theta5_result),cos(theta5_result),0,0;          0,0,0,1];
+T56 = [cos(theta6_result),-sin(theta6_result),0,0;    0,0,1,0;    -sin(theta6_result),-cos(theta6_result),0,0;          0,0,0,1];
+
+P0_6 =  T01 * T12 * T23 * T34 * T45 * T56;
