f309b9d8609a212620a469a665802d6e4ed727ea
WUT_Ruige_Lee <295054118@whut.edu.cn>
2018-10-29 10:31:37 +0800

    EDGE逻辑生成脚本

---
+++ matlab/anno_robotic_arm_edge_create.m
@@ -0,0 +1,19 @@
+% 安诺6轴机械臂PRM算法EDGE查找表生成
+% 武汉理工大学
+% 李锐戈
+
+precision = 5;
+space_length = 1000;
+edge_require = 1024;
+
+Pose = [0,0,0,0,0,0];
+Pose_cnt = 1;
+
+
+edge_cnt = 0;
+X = 0;
+Y = 0;
+Z = 0;
+
+
+
