41694348f26c1f6f68491df7723c57bec4813720
WUT_DDK_SFB <295054118@whut.edu.cn>
2018-11-02 23:02:44 +0800

    清理代码

--- Klampt/main.py
+++ Klampt/main.py
@@ -17,7 +17,7 @@ def make_testing_mesh(world):
 
 				grid.loadFile("terrains/cube.off")
 
-				grid.transform([0.0319,0,0,0,0.0319,0,0,0,0.0319],[0.032*x - 0.512,0.032*y- 0.512,0.032*z])
+				grid.transform([0.032,0,0,0,0.032,0,0,0,0.032],[0.032*x - 0.512,0.032*y- 0.512,0.032*z])
 					
 
 	
@@ -26,7 +26,7 @@ def make_testing_mesh(world):
 				Mesh = world.makeTerrain("Grid" + str(x) + "," + str(y) + "," + str(z))
 
 				Mesh.geometry().set(grid)
-				Mesh.appearance().setColor(0.6,0.2,0.3,0.2)
+				Mesh.appearance().setColor(0.3,0.1,0.1,0.5)
 	return 
 
 
@@ -49,66 +49,26 @@ if __name__ == "__main__":
 	vis.add("world",world)
 	sim = Simulator(world)
 	robot = world.robot(0)
-	#endeffectors = [robot.numLinks()-1]
+
 	collisionTest = WorldCollider(world)
-	#controller = sim.getController(0)
 	
 	robotPose = RobotPoser(robot)
-	print robotPose.get()
 	
-
+	#print robotPose.get()
 	
-	
-	#sim.reset()
-	#sim.getStatus()
-	#base = sim.body(world.robotLink(0,0))
-	#shoulder = sim.body(world.robotLink(0,1))
-	#arm = sim.body(world.robotLink(0,2))
-	#elbow = sim.body(world.robotLink(0,3))
-	#wrist = sim.body(world.robotLink(0,4))
-	#finger = sim.body(world.robotLink(0,5))
-	#tool = sim.body(world.robotLink(0,6))
-	#body = sim.body(world.rigidObject(1))
-	#body.applyForceAtPoint ([0,2000,200],[0,0,0])
-
-	#print base.getVelocity()
-	#print shoulder.getVelocity()
-	#print arm.getVelocity()
-	#print elbow.getVelocity()
-	#print wrist.getVelocity()
-	#print finger.getVelocity()
-	#print tool.getVelocity()
-	
-	vis.show()
-
-	#shoulder.enableDynamics(False)
-	#arm.enableDynamics(False)
-	#elbow.enableDynamics(False)
-	#wrist.enableDynamics(False)
-	#finger.enableDynamics(False)
-	#tool.enableDynamics(False)
-	
-	#shoulder.setVelocity((1,0,0),(0,0,0))
-	#arm.setVelocity((0,0,0),(0,0,0))
-	#elbow.setVelocity((0,0,0),(0,0,0))
-	#wrist.setVelocity((0,0,0),(0,0,0))
-	#finger.setVelocity((0,0,0),(0,0,0))
-	#tool.setVelocity((0,0,0),(0,0,0))
-
-
-	#t0 = time.time()
 	for i in range(0,100):
 		#vis.shown()
-		#sim.simulate(0.01)
-		robotPose.set([0,0.0157*i,-0.0157*i,-0.0157*i,0,0,0])
+
+		robotPose.set([0,-0.0157*i,-0.0157*i,-0.0157*i,-0.0157*i,-0.0157*i,0])
 		collisionTest = WorldCollider(world)
-		#for i,j in  collisionTest.robotTerrainCollisions(0):
+
+		cnt = 0;
 		for i,j in collisionTest.robotTerrainCollisions(0):
-			print j.getName()
+			#print j.getName()
+			cnt = cnt + 1;
+		print "cnt in this frame"
+		print cnt
+
 			#pass
 			
-		
-		#t1 = time.time()
-	#update your code here	       
-		time.sleep(0.1)
-		#t0 = t1
+
