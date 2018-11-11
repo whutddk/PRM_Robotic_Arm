aac6f4ea86c51730f3889ca98ad4fdf42a9c9dc0
WUT_DDK_SFB <295054118@whut.edu.cn>
2018-11-11 16:41:45 +0800

    交付1颗粒综合

--- Klampt/truetable/resoure/createEpVerilog.py
+++ Klampt/truetable/resoure/createEpVerilog.py
@@ -1,11 +1,17 @@
 import sys
 import time
 
-for k in range(0,1024):
-	with open('./singleVerilog/singleV'+str(k),'w') as vFile:
-		with open('./single/result/singleRes'+str(k),'r') as epressFile:
-			data = epressFile.readlines()[3:-1]
-			for line in data:
-				vFile.write(line)
+with open ('./singleVerilog/singleV.v','w') as vFile:
+	for k in range(0,1024):
+		with open('./singleVerilog/singleV'+str(k),'r') as epressFile:
+			vFile.write('casez({A,B,C,D,E,F,G,H,I,J,K,L,M,N,O})\n')
+			lines = epressFile.readlines()
+			string = ''
+			for data in lines:
+				string = string + '	15\'b'+data[0:15]+',\n'
+			string = string[0:-2]
+			vFile.write(string)
+			vFile.write(': edge_mask['+str(k)+'] <= 1\'b1')
+			vFile.write(';\n	default:edge_mask['+str(k)+'] <= 1\'b0;\nendcase\n\n')
 
-			#vFile.write(data)
+				#vFile.write(data)
