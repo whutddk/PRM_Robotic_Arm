4a6a1b358c3ee4c3bbc73b896be19e9dbee5d942
WUT_DDK_SFB <295054118@whut.edu.cn>
2018-11-11 16:14:09 +0800

    开始组合1粒度verilog

--- Klampt/truetable/resoure/createEpVerilog.py
+++ Klampt/truetable/resoure/createEpVerilog.py
@@ -2,10 +2,10 @@ import sys
 import time
 
 for k in range(0,1024):
-with open('./singleVerilog/singleV','w') as vFile:
-
+	with open('./singleVerilog/singleV'+str(k),'w') as vFile:
 		with open('./single/result/singleRes'+str(k),'r') as epressFile:
-			vFile.write('casez(edge_mask['+str(k)+'])\n')
-			vFile.write(epressFile.readlines()[3:-1])
+			data = epressFile.readlines()[3:-1]
+			for line in data:
+				vFile.write(line)
 
 			#vFile.write(data)
