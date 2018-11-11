153e374a9e5bf35353741f61ee8ba8888a5ee501
WUT_DDK_SFB <295054118@whut.edu.cn>
2018-11-11 11:06:29 +0800

    打128*8真值查找表

--- Klampt/truetable/resoure/createEpresso.py
+++ Klampt/truetable/resoure/createEpresso.py
@@ -19,41 +19,34 @@ def load_trueTable():
 def write_verilog():
 	global trueTable
 
-	with open('./epressoTable','w') as epressFile:
+	for k in range(0,8):
+		with open('./128/epressoTable'+str(k),'w') as epressFile:
 
-		# for i in range(0,32768):	#all case all pix
-		# 	#strSyntax =  '15\'d'+ str(i) + ': edgeMask <= 1034\'b'
-		# 	strData = ''
-		# 	for j in range(0,1024): # all edge
-		# 		strData = strData + str(trueTable[i][j]) + ','
-		# 	#strSyntax = strSyntax + strData + ';\n'
-		# 	strData = strData +'\n'
-		# 	verilogFile.write(strData)
-		# 	#print strSyntax
+
 		#epressFile.write('.i 15\n.o 1024\n')
-		epressFile.write('.i 15\n.o 32\n')
+			epressFile.write('.i 15\n.o 128\n')
 
-		#for i in range(0,32768): #all pix
-		for i in range(0,32768): #all pix
-			logicI = bin(i)
-			logicInput = ''
-			inputlen = len(logicI)
+			#for i in range(0,32768): #all pix
+			for i in range(0,32768): #all pix
+				logicI = bin(i)
+				logicInput = ''
+				inputlen = len(logicI)
 
-			logicInput = logicI[2:inputlen]
-			while(inputlen < 17 ):
-				logicInput = '0'+ logicInput
-				inputlen = inputlen + 1
-			logicInput = logicInput +' '
-			#print (logicInput)
-			logicOutput = ''
-			#for j in range(0,1024): #all edge
-			for j in range(0,32): #all edge
-				logicOutput = logicOutput+str(trueTable[i][j])
-			logicOutput = logicOutput +'\n'
-				#strData = 'assign edge_mask['+str(j)+']=('
-			pass
-				
-			epressFile.write(logicInput+logicOutput)
+				logicInput = logicI[2:inputlen]
+				while(inputlen < 17 ):
+					logicInput = '0'+ logicInput
+					inputlen = inputlen + 1
+				logicInput = logicInput +' '
+				#print (logicInput)
+				logicOutput = ''
+				#for j in range(0,1024): #all edge
+				for j in range(128*k+0,128*k+128): #all edge
+					logicOutput = logicOutput+str(trueTable[i][j])
+				logicOutput = logicOutput +'\n'
+					#strData = 'assign edge_mask['+str(j)+']=('
+				pass
+					
+				epressFile.write(logicInput+logicOutput)
 
 
 		pass
